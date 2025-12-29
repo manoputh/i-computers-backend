38fb25d6f58cce5f238a53bc558eb64cec3f77b3
manoputh <e21292@eng.pdn.ac.lk>
2025-12-30 01:21:31 +0530

    refactor: clean up product controller and model code for better readability✏️

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,117 +2,124 @@ import Product from "../models/Product.js";
 import { isAdmin } from "./userController.js";
 
 export function createProduct(req, res) {
-  if (!isAdmin(req)) {
-    return res.status(403).json({
-      message: "Access denied. Admins only.",
-    });
-  }
-
-  const product = new Product(req.body);
-
-  product
-    .save()
-    .then(() => {
-      res.json({
-        message: "Product created successfully",
+   if (!isAdmin(req)) {
+      return res.status(403).json({
+         message: "Access denied. Admins only.",
       });
-      console.log("Product created successfully");
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: "Error creating product",
-        error: error.message,
+   }
+
+   const product = new Product(req.body);
+
+   product
+      .save()
+      .then(() => {
+         res.json({
+            message: "Product created successfully",
+         });
+         console.log("Product created successfully");
+      })
+      .catch((error) => {
+         res.status(500).json({
+            message: "Error creating product",
+            error: error.message,
+         });
+         console.log("Error creating product:", error.message);
       });
-      console.log("Error creating product:", error.message);
-    });
 }
 
 export async function getAllProducts(req, res) {
-  try {
-    if (isAdmin(req)) {
-      // Product.find().then(
-      //     (products)=>{
-      //         res.json(products)
-      //     }
-      // ).catch(
-      //     (error)=>{
-      //         res.status(500).json({
-      //             message : "Error fetching products",
-      //             error : error.message
-      //         });
-      //     }
-      // )
+   try {
+      if (isAdmin(req)) {
+         // Product.find().then(
+         //     (products)=>{
+         //         res.json(products)
+         //     }
+         // ).catch(
+         //     (error)=>{
+         //         res.status(500).json({
+         //             message : "Error fetching products",
+         //             error : error.message
+         //         });
+         //     }
+         // )
 
-      const products = await Product.find();
-    } else {
-      Product.find({ isAvailable: true })
-        .then((products) => {
-          res.json(products);
-        })
-        .catch((error) => {
-          res.status(500).json({
-            message: "Error fetching products",
-            error: error.message,
-          });
-        });
-    }
-  } catch (error) {
-    res.status(500).json({
-      message: "Error fetching products",
-      error: error.message,
-    });
-  }
+         const products = await Product.find();
+         //  if (products.length === 0) {
+         //     return res.status(404).json({
+         //        message: "No products found",
+         //     });
+         //  }
+         res.json(products);
+      } else {
+         Product.find({ isAvailable: true })
+            .then((products) => {
+               res.json(products);
+               console.log("Fetched products for non-admin user:", products);
+            })
+            .catch((error) => {
+               res.status(500).json({
+                  message: "Error fetching products",
+                  error: error.message,
+               });
+            });
+      }
+   } catch (error) {
+      res.status(500).json({
+         message: "Error fetching products",
+         error: error.message,
+      });
+   }
 }
 
 export function deleteProduct(req, res) {
-  if (!isAdmin(req)) {
-    return res.status(403).json({
-      message: "only admin can delete products.",
-    });
-  }
+   if (!isAdmin(req)) {
+      return res.status(403).json({
+         message: "only admin can delete products.",
+      });
+   }
 
-  const productID = req.params.productID;
+   const productID = req.params.productID;
 
-  Product.deleteOne({ productID: productID }).then(() => {
-    res.json({
-      message: "Product deleted successfully",
-    });
-  });
+   Product.deleteOne({ productID: productID }).then(() => {
+      res.json({
+         message: "Product deleted successfully",
+      });
+   });
 }
 
 export function updateProduct(req, res) {
-  if (!isAdmin(req)) {
-    return res.status(403).json({
-      message: "only admin can update products.",
-    });
-  }
+   if (!isAdmin(req)) {
+      return res.status(403).json({
+         message: "only admin can update products.",
+      });
+   }
 
-  const productID = req.params.productID;
+   const productID = req.params.productID;
 
-  Product.updateOne({ productID: productID }, req.body).then(() => {
-    res.json({
-      message: "Product updated successfully",
-    });
-  });
+   Product.updateOne({ productID: productID }, req.body).then(() => {
+      res.json({
+         message: "Product updated successfully",
+      });
+   });
 }
 
 export function getProductByID(req, res) {
-  const productID = req.params.productID;
+   const productID = req.params.productID;
 
-  Product.findOne({ productID: productID })
-    .then((product) => {
-      if (product == null) {
-        res.status(404).json({
-          message: "Product not found",
-        });
-      } else {
-        res.json(product);
-      }
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: "Error fetching product",
-        error: error.message,
+   Product.findOne({ productID: productID })
+      .then((product) => {
+         if (product == null) {
+            res.status(404).json({
+               message: "Product not found",
+            });
+         } else {
+            res.json(product);
+         }
+      })
+      .catch((error) => {
+         res.status(500).json({
+            message: "Error fetching product",
+            error: error.message,
+         });
       });
-    });
 }
--- models/Product.js
+++ models/Product.js
@@ -1,58 +1,58 @@
 import mongoose, { model } from "mongoose";
 
 const productSchema = new mongoose.Schema({
-  productID: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  altNames: {
-    type: [String],
-    default: [],
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  labelledPrice: {
-    type: Number,
-    required: true,
-  },
-  images: {
-    type: [String],
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  model: {
-    type: String,
-    required: true,
-    default: "Standard",
-  },
-  brand: {
-    type: String,
-    required: true,
-    default: "Generic",
-  },
-  stock: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  isAvailable: {
-    type: Boolean,
-    default: true,
-  },
+   productID: {
+      type: String,
+      required: true,
+      unique: true,
+   },
+   name: {
+      type: String,
+      required: true,
+   },
+   altNames: {
+      type: [String],
+      default: [],
+   },
+   description: {
+      type: String,
+      required: true,
+   },
+   price: {
+      type: Number,
+      required: true,
+   },
+   labelledPrice: {
+      type: Number,
+      required: true,
+   },
+   images: {
+      type: [String],
+      required: true,
+   },
+   category: {
+      type: String,
+      required: true,
+   },
+   model: {
+      type: String,
+      required: true,
+      default: "Standard",
+   },
+   brand: {
+      type: String,
+      required: true,
+      default: "Generic",
+   },
+   stock: {
+      type: Number,
+      required: true,
+      default: 0,
+   },
+   isAvailable: {
+      type: Boolean,
+      default: true,
+   },
 });
 
 // Create a Mongoose model for the product schema
